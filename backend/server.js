4a76098a3072e9ab983e92331ba222c7e22c9a00
Adam Ocheri <adamotchery@gmail.com>
2023-02-02 16:19:29 +0200

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,11 @@ import { errorHandler } from './middleware/errorHandler.js';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import cors from 'cors';
+import path from 'path';
+//fix Node's "path" to support ESModules instead of CJS
+import * as url from 'url';
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 dotenv.config();
 connectDB();
 const app = express();
@@ -14,6 +19,19 @@ app.use(express.json());
 app.use("/api/projects", ProjectRouter);
 app.use("/api/user", UserRouter);
 app.use(errorHandler);
+//Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/strategize/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../', 'frontend', 'strategize', 'build', 'index.html'));
+    });
+}
+else {
+    app.get('/', (req, res) => {
+        res.send('Please set to production environment');
+    });
+}
+//Start server
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
--- backend/server.ts
+++ backend/server.ts
@@ -1,3 +1,4 @@
+
 import express from 'express';
 import ProjectRouter from './routes/projectsRoute.js';
 import UserRouter from './routes/userRoute.js';
@@ -5,11 +6,19 @@ import { errorHandler } from './middleware/errorHandler.js';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import cors from 'cors'
+import path from 'path';
+
+//fix Node's "path" to support ESModules instead of CJS
+import * as url from 'url';
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+
 
 dotenv.config();
 connectDB();
 
-const app : express.Application = express();
+const app  = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -18,8 +27,25 @@ app.use("/api/projects", ProjectRouter);
 app.use("/api/user", UserRouter);
 app.use(errorHandler);
 
-const PORT : any = process.env.PORT || 4000;
+//Serve Frontend
+if (process.env.NODE_ENV === 'production')
+{
+    app.use(express.static(path.join(__dirname, '../frontend/strategize/build')));
 
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, '../', 'frontend', 'strategize', 'build', 'index.html')
+        );
+    });
+}
+else{
+    app.get('/', (req,res) => {
+        res.send('Please set to production environment')
+    })
+}
+
+//Start server
+const PORT : any = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 })
